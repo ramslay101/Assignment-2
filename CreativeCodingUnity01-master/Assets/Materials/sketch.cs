5ee6f58335136621571eae89519b11ac50f27b52
ramslay101 <ramslay101@gmail.com>
2016-05-20 12:05:03 +1200

    Absolutely Prefeb episode
    add sketch code script and instantiate the spin cube and tell it what
    to do.

---
+++ CreativeCodingUnity01-master/Assets/Materials/sketch.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sketch : MonoBehaviour {
+
+    public GameObject myprefab;
+
+    void Start()
+    {
+        int totalcubes = 8;
+
+        int totaldistance = 10;
+        for (int i = 0; i < totalcubes; i++ )
+        {
+            float perc = i / (float)totalcubes;
+        
+        float x = perc * totaldistance;
+        float y = 10.0f;
+        float z = 0.0f;
+
+        var newCube = (GameObject)Instantiate(myprefab, new Vector3(x, y, z), Quaternion.identity);
+        newCube.GetComponent<CubeCode>().SetSize(1.0f - perc);
+        newCube.GetComponent<CubeCode>().RotateSpeed = perc;
+        }
+	}
+	
+	void Update () {
+	
+	}
+}
