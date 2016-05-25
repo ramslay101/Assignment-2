78ba45b1cecb8ce27ecec4bac06d11154c398a2b
ramslay101 <ramslay101@gmail.com>
2016-05-25 16:41:26 +1200

    Add Google sheet data
    Add google sheet to azure and then read table from azure to unity
    project

--- CreativeCodingUnity01-master/Assets/testsketch.cs
+++ CreativeCodingUnity01-master/Assets/testsketch.cs
@@ -1,15 +1,12 @@
 using UnityEngine;
 using Pathfinding.Serialization.JsonFx; //make sure you include this using
-using System.Collections;
 
-public class exampleSketch : MonoBehaviour
-{
-    public GameObject myprefab;
-    public string _WebsiteURL = "http://camsbombapp.azurewebsites.net/tables/products?zumo-api-version=2.0.0";
+public class testsketch : MonoBehaviour {
+    public GameObject myPrefab;
+    public string _WebsiteURL = "http://camsbombapp.azurewebsites.net/tables/assignmentdata?zumo-api-version=2.0.0";
 
-    void Start()
+    void Start ()
     {
-
         string jsonResponse = Request.GET(_WebsiteURL);
 
         //Just in case something went wrong with the request we check the reponse and exit if there is no response.
@@ -21,31 +18,31 @@ public class exampleSketch : MonoBehaviour
         //We can now deserialize into an array of objects - in this case the class we created. The deserializer is smart enough to instantiate all the classes and populate the variables based on column name.
         Product[] products = JsonReader.Deserialize<Product[]>(jsonResponse);
 
-        int totalcubes = 8;
+        int totalcubes = products.GetLength(0);
 
-        int totaldistance = 10;
+        float totaldistance = 7;
         int i = 0;
+
+        //We can now loop through the array of objects and access each object individually
         foreach (Product product in products)
         {
             //Example of how to use the object
-            Debug.Log("This products name is: " + product.ProductName);
+            Debug.Log("This products name is: " + product.Title);
 
-           
-            
                 float perc = i / (float)totalcubes;
                 i++;
-                float x = perc * totaldistance;
+                float sin = Mathf.Sin(perc * Mathf.PI / 2);
+
+                float x = 4.0f + sin * totaldistance;
                 float y = 10.0f;
                 float z = 0.0f;
 
-                var newCube = (GameObject)Instantiate(myprefab, new Vector3(x, y, z), Quaternion.identity);
-                newCube.GetComponent<CubeCode>().SetSize(1.0f - perc);
-                newCube.GetComponent<CubeCode>().RotateSpeed = perc;
-                newCube.GetComponentInChildren<TextMesh>().text = product.ProductName;
-
-            
+                var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+                newCube.GetComponent<CubeCode>().SetSize(1f * (1.0f - perc));
+                newCube.GetComponent<CubeCode>().RotateSpeed = .2f + perc * 2.0f;
+                newCube.GetComponentInChildren<TextMesh>().text = product.Title;
         }
-    }
+	}
 	
 	// Update is called once per frame
 	void Update () {
